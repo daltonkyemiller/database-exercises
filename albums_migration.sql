d0a2843aeee6a7b9bc837f25aca42e3acac35b9a
Dalton Miller <dalton.kye.miller@gmail.com>
2022-07-07 07:28:25 -0500

    changed id name

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,7 +1,7 @@
 USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
-  id serial PRIMARY KEY,
+  id id PRIMARY KEY,
   artist VARCHAR(255),
   name VARCHAR(255),
   release_date INT,
